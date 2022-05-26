1782f190d285f1c59dd7b7e8ed141bac9209a958
Yu357 <forestgreen14689@gmail.com>
2022-05-26 20:29:21 +0900

    feat: Routing

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import {BrowserRouter, Route, Routes} from 'react-router-dom'
+import Top from './screens/Top'
+import About from './screens/About'
+import Contact from './screens/Contact'
+import NotFound from './screens/NotFound'
+
+
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
-);
+)
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Top/>}/>
+        <Route path='/about' element={<About/>}/>
+        <Route path='/contact' element={<Contact/>}/>
+        <Route path='*' element={<NotFound/>}/>
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
-);
+)
---
+++ src/screens/Top.tsx
@@ -0,0 +1,9 @@
+function Top() {
+  return (
+    <div>
+      <h2>Top</h2>
+    </div>
+  )
+}
+
+export default Top
