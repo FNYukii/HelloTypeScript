cfa370958d17f86a8386e582d043bfe5b104a80d
Yu357 <forestgreen14689@gmail.com>
2022-06-30 10:51:46 +0900

    feat: HeaderにPure CSSを適用

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,13 @@
 import { NavLink } from 'react-router-dom'
+import '../styles/header.css'
 
 function Header() {
   return (
     <header>
-      <div>
-        <a href='/'>Hello React</a>
+      <div className='large-container'>
+        <a href='/' className='logo'>Hello React</a>
 
-        <ul>
+        <ul className='global-nav'>
           <li><NavLink to='/'>Top</NavLink></li>
           <li><NavLink to='/about'>About</NavLink></li>
           <li><NavLink to='/contact'>Contact</NavLink></li>
