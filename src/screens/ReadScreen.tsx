75202e6ef64b1c1b1f62d0f90d79c1451ce6f016
Yu357 <forestgreen14689@gmail.com>
2022-09-27 14:43:27 +0900

    fix: スタイル修正

--- src/screens/ReadScreen.tsx
+++ src/screens/ReadScreen.tsx
@@ -1,8 +1,13 @@
 function ReadScreen() {
+
+    const fruits: string[] = ["apple", "orange"]
+
     return (
         <main>
             <div className="large-container">
                 <h2>Read</h2>
+
+                <p>{fruits[0]}</p>
             </div>
         </main>
     )
