e547927c4104ba2c3878e183b6742e182edea8df
Yu357 <forestgreen14689@gmail.com>
2022-09-29 10:15:56 +0900

    clean

--- src/view-models/UsersViewModel.tsx
+++
@@ -1,9 +0,0 @@
-import { useEffect, useState } from 'react'
-import User from '../entities/User'
-
-import progressView from '../images/progressView.svg'
-
-import { FireUser } from '../utilities/FireUser'
-
-import { collection, query, getDocs, onSnapshot } from "firebase/firestore"
-import { db } from "../utilities/firebase"
